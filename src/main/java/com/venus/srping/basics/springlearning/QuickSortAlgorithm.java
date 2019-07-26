4e8561e2f74f52c08139c20ac2d5bd8b21b75bcf
venuschau <Venus.Chau@pccw.com>
2019-07-26 15:12:32 +0800

    debug mode

--- src/main/java/com/venus/srping/basics/springlearning/QuickSortAlgorithm.java
+++ src/main/java/com/venus/srping/basics/springlearning/QuickSortAlgorithm.java
@@ -1,5 +1,10 @@
 package com.venus.srping.basics.springlearning;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm{
 	public int[] sort(int[] numbers) {
 		return numbers;
